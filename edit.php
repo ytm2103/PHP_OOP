b82c1b43edf084535946fa38d4672fff24080d94
Atsushi <atsushi88885@gmail.com>
2019-04-26 18:48:50 +0900

    編集ページ作成

---
+++ edit.php
@@ -0,0 +1,42 @@
+<?php
+    require_once('function.php');
+    require_once('Models/Todo.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TODO APP</title>
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header class="px-5 bg-primary">
+        <nav class="navbar navbar-dark">
+            <a href="index.php" class="navbar-brand">TODO APP</a>
+            <div class="justify-content-end">
+                <span class="text-light">
+                    SeedKun
+                </span>
+            </div>
+        </nav>
+    </header>
+    <main class="container py-5">
+        <section>
+            <form class="form-row" action="update.php" method="POST">
+                <div class="col-12 col-md-9 py-2">
+                    <input type="text" name="task" class="form-control" placeholder="ADD TODO">
+                </div>
+                <div class="py-2 col-md-3 col-12">
+                    <button type="submit" class="col-12 btn btn-primary btn-block">ADD</button>
+                </div>
+            </form>
+        </section>
+    </main>
+    
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
